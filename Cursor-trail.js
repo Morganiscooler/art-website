1e9cdb6fdc0f8455a110e973c838032c25355bf1
Lukinator3000 <97560303+Lukinator3000@users.noreply.github.com>
2024-07-04 19:44:05 -0400

    Add files via upload

---
+++ Cursor-trail.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", (event) => {
+    new cursoreffects.fairyDustCursor({
+        colors: ["#ff6392", "##ff6f59", "##ff9c40", "#fff86b","#7cff78","#78ffe6","#78dbff","#789cff","#a078ff","#d978ff","#ff78e4"],
+      });
+});
+// Color array is not working !! Fix this later...
